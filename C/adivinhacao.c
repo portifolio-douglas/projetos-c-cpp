88ab37ec06f499715ff43d7fb1fb3d7767b6b541
Douglas Henrique <douglas.ferreira@inventvision.com.br>
2024-03-15 14:56:22 -0300

    Added loop 'for' in code

--- C/adivinhacao.c
+++ C/adivinhacao.c
@@ -10,23 +10,26 @@ int main() {
 
     int chute;
 
-    printf("Qual é o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi %d\n", chute);
-    
-    if(chute == numerosecreto) {
-        printf("Parabéns! Você acertou!\n");
-        printf("Jogue de novo, você é um bom jogador!\n");
-    }
+    for (int i = 1; i <= 3; i++){
+        printf("Qual é o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi %d\n", chute);
+        
+        if(chute == numerosecreto) {
+            printf("Parabéns! Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador!\n");
+        }
 
-    else {
-        
-        if(chute > numerosecreto) {
-             printf("Seu chute foi maior que o número secreto\n");
-        }
-        
-        if(chute < numerosecreto) {
-             printf("Seu chute foi menor que o número secreto\n");
+        else {
+            
+            if(chute > numerosecreto) {
+                printf("Seu chute foi maior que o número secreto\n");
+            }
+            
+            if(chute < numerosecreto) {
+                printf("Seu chute foi menor que o número secreto\n");
+            }
         }
     }
+    printf("Fim de jogo!\n");
 }
