1c13440c2c48cecda7af42368bd947c48f2aee0e
Douglas Henrique <douglas.ferreira@inventvision.com.br>
2024-03-20 13:48:38 -0300

    Added double loop logic

--- C/jogo_forca/forca.c
+++ C/jogo_forca/forca.c
@@ -6,19 +6,39 @@ int main() {
     sprintf(palavra_secreta, "MELANCIA");
 
     int acertou = 0;
-    int enforcou = 1;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
 
     do {
-        char chute;
-
-        printf("Qual a letra? ");
-        scanf("%c", &chute);
 
         for (int i = 0; i < strlen(palavra_secreta); i++) {
-            if (palavra_secreta[i] == chute) {
-                printf("A posição %d tem essa letra!\n", i);
+
+            int achou = 0;
+            
+            for (int j = 0; j < tentativas; j++) {
+                if (chutes[j] == palavra_secreta[i]) {
+                    achou = 1;
+                    break;
+                }
             }
+
+            if (achou) {
+                printf("%c ", palavra_secreta[i]);
+            } else {
+                printf("_ ");
+            }          
         }
+        printf("\n");
+        
+        char chute;
+        printf("Qual letra? ");
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+
     } while (!acertou && !enforcou);
     
 }
