e3eb6d1dc769fa314e04b64e79d877735ae161b7
Douglas Henrique <douglas.ferreira@inventvision.com.br>
2024-03-22 15:10:24 -0300

    Added new files to the project

---
+++ C/atividades.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int soma(int inteiros[2], int tamanho) {
+    
+    return inteiros[0] + inteiros[1];
+}
---
+++ C/jogo_forca/forca.h
@@ -0,0 +1,7 @@
+void abertura();
+void chuta();
+int ja_chutou(char letra);
+void desenha_forca();
+int acertou();
+int enforcou();
+void escolhe_palavra();
