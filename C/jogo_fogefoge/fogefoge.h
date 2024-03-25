843ba719271fb90b8a5e665ef1a4b5cec3bfd012
Douglas Henrique <douglas.ferreira@inventvision.com.br>
2024-03-25 13:20:40 -0300

    Added new features to the game

--- C/jogo_fogefoge/fogefoge.h
+++ C/jogo_fogefoge/fogefoge.h
@@ -1,2 +1,10 @@
+#define CIMA 'w'
+#define BAIXO 's'
+#define DIREITA 'd'
+#define ESQUERDA 'a'
+
 int acabou();
 void move(char direcao);
+ 
+int ehdirecao(char direcao);
+void fantasmas();
--- C/jogo_fogefoge/mapa.h
+++ C/jogo_fogefoge/mapa.h
@@ -1,3 +1,9 @@
+#define HEROI '@'
+#define VAZIO '.'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+#define FANTASMA 'F'
+
 struct mapa {
 	char** matriz;
 	int linhas;
@@ -18,4 +24,15 @@ struct posicao {
 
 typedef struct posicao POSICAO;
 
-void encontramapa(MAPA* m, POSICAO* p, char c);
+int encontramapa(MAPA* m, POSICAO* p, char c);
+
+int ehvalida(MAPA* m, int x, int y);
+int ehparede(MAPA* m, int x, int y);
+int ehpersonagem(MAPA* m, char personagem, int x, int y);
+
+void andanomapa(MAPA* m, int xorigem, int yorigem, 
+	int xdestino, int ydestino);
+
+void copiamapa(MAPA* destino, MAPA* origem);
+
+int podeandar(MAPA* m, char personagem, int x, int y);
