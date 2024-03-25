7c216e9f99ccf158aef1924621881fe09d9a243f
Douglas Henrique <douglas.ferreira@inventvision.com.br>
2024-03-25 14:39:42 -0300

    New features in the game

--- C/atividades.c
+++ C/atividades.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
-int soma(int inteiros[2], int tamanho) {
-    
-    return inteiros[0] + inteiros[1];
+int fatorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * fatorial(n - 1);
+}
+
+int main() {
+    int numero;
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &numero);
+    printf("O fatorial de %d é %d\n", numero, fatorial(numero));
+    return 0;
 }
